e0d7b2c44d510febe2fd5f0a46742beabb805c2f
Alessio Bernabei <alessio.bernabei@studenti.unipd.it>
2026-07-02 23:21:51 +0200

    aggiunti log per trasferimenti pezzi tra stazioni/buffer e per stato di raffreddamento GOM

--- libs/controller/src/controller.c
+++ libs/controller/src/controller.c
@@ -9,7 +9,7 @@ static void unload_all(cella_meccatronica *c){
         quality_control(&(c->pezzi_completati), &(c->pezzi_scartati), (&c->list_head), pgom);
         if(pgom->stato == OK){
             record_piece_done(pgom);
-            log_info_f(c->tick_corrente, "Pezzo %d completato (lead time %d tick)", pgom->id_pezzo, pgom->lead_time);
+            log_info_f(c->tick_corrente, "Pezzo %d scaricato dal GOM e completato (lead time %d tick)", pgom->id_pezzo, pgom->lead_time);
         } else {
             record_piece_scrap(pgom);
             log_warning_f(c->tick_corrente, "Pezzo %d scartato al GOM, reinserito in produzione", pgom->id_pezzo);
@@ -19,8 +19,12 @@ static void unload_all(cella_meccatronica *c){
     //valuto se il buffer può ricevere un altro pezzo, poi scarico il pezzo dalla pressa
     if(is_full(c->buf_gom)==0){
         pezzo *ppres = pressa_unload(c->pressa, c->tick_corrente);
-        if(ppres != NULL)
+        if(ppres != NULL){
+        // log_info deve essere fatto dentro if sennò molte volte prenderebbe NULL e quindi crash
+        log_info_f(c->tick_corrente, "Pezzo %d scaricato dalla pressa", ppres->id_pezzo);
         new_item(ppres, c->buf_gom);
+        log_info_f(c->tick_corrente, "Pezzo %d inserito nel buffer GOM", ppres->id_pezzo);
+        }
     } else {
         record_block();
         log_warning_f(c->tick_corrente, "Buffer GOM pieno: scarico dalla pressa bloccato");
@@ -28,8 +32,12 @@ static void unload_all(cella_meccatronica *c){
     //valuto se il buffer può ricevere un altro pezzo, poi scarico il pezzo dalla laminazione
     if(is_full(c->buf_pressa)==0){
         pezzo *plam = laminazione_unload(c->laminazione, c->tick_corrente);
-        if(plam != NULL)
+        if(plam != NULL){
+        // log_info deve essere fatto dentro if sennò molte volte prenderebbe NULL e quindi crash
+        log_info_f(c->tick_corrente, "Pezzo %d scaricato dalla laminazione", plam->id_pezzo);
         new_item(plam, c->buf_pressa);
+        log_info_f(c->tick_corrente, "Pezzo %d inserito nel buffer pressa", plam->id_pezzo);
+        }
     } else {
         record_block();
         log_warning_f(c->tick_corrente, "Buffer pressa pieno: scarico dalla laminazione bloccato");
@@ -37,8 +45,12 @@ static void unload_all(cella_meccatronica *c){
     // valuto se il buffer può ricevere un altro pezzo, poi scarico il pezzo dall'agv
     if(is_full(c->buf_lam)==0){
         pezzo *pagv = agv_unload(c->agv);
-        if(pagv != NULL)
+        if(pagv != NULL){
+        // log_info deve essere fatto dentro if sennò molte volte prenderebbe NULL e quindi crash
+        log_info_f(c->tick_corrente, "Pezzo %d scaricato dall'AGV", pagv->id_pezzo);
         new_item(pagv, c->buf_lam);
+        log_info_f(c->tick_corrente, "Pezzo %d inserito nel buffer laminazione", pagv->id_pezzo);
+        }
     } else {
         record_block();
         log_warning_f(c->tick_corrente, "Buffer laminazione pieno: scarico dall'AGV bloccato");
@@ -49,38 +61,54 @@ static void load_all(cella_meccatronica *c){
     // prendo dal buffer il primo elemento (puntatore a pezzo) e valuto se esiste o è NULL
     if(gom_is_free(c->gom) && !is_empty(c->buf_gom)){
         pezzo *pgom = take_item(c->buf_gom);
-        if(pgom != NULL)
-        gom_load(c->gom, pgom, c->tick_corrente);
+        if(pgom != NULL){
+            gom_load(c->gom, pgom, c->tick_corrente);
+            log_info_f(c->tick_corrente, "Pezzo %d caricato sul GOM", pgom->id_pezzo);
+        }
     }
     // prendo dal buffer il primo elemento (puntatore a pezzo) e valuto se esiste o è NULL
     if(pressa_is_free(c->pressa) && !is_empty(c->buf_pressa)){
         pezzo *ppres = take_item(c->buf_pressa);
-        if(ppres != NULL)
-        pressa_load(c->pressa, ppres, c->tick_corrente);
+        if(ppres != NULL) {
+            pressa_load(c->pressa, ppres, c->tick_corrente);
+            log_info_f(c->tick_corrente, "Pezzo %d caricato sulla pressa", ppres->id_pezzo);
+        }
+        
     }
     // prendo dal buffer il primo elemento (puntatore a pezzo) e valuto se esiste o è NULL
     if(laminazione_is_free(c->laminazione) && !is_empty(c->buf_lam)){
         pezzo *plam = take_item(c->buf_lam);
-        if(plam != NULL)
+        if(plam != NULL){
         laminazione_load(c->laminazione, plam, c->tick_corrente);
+        log_info_f(c->tick_corrente, "Pezzo %d caricato sulla laminazione", plam->id_pezzo);
+        }
     }
      
     //uso un case switch per selezionare politica di ctrl
     if(strcmp(c->param.politica_controllo, "fcfs")){
         // trovo il primo pezzo che non ha ancora iniziato lavorazione e dico al gom di prendere lo stampo
         pezzo *pagv = first_pezzo_with_status(c->list_head, CREATED);
-        if(pagv != NULL)
+        if(pagv != NULL){
         //non serve una condizione di controllo se agv è libero, è integrata in agv_get_mold.
         //agv non parte se non è libero
         agv_get_mold(c->agv, pagv, c->param.tempo_agv, c->tick_corrente);
+        log_info_f(c->tick_corrente, "Poltica di controllo FCFS: pezzo %d caricato sull'AGV per trasporto", pagv->id_pezzo);
+        }
     }
     //else if(strcmp(c->param.politica_controllo,"priorità"))
+
+    //log_info_f(c->tick_corrente, "Politica di controllo con priorità: pezzo %d caricato sull'AGV per trasporto", pagv->id_pezzo);
     
 } 
-
+// per mettere i messaggi di log solo nel controllore e non nelle singole macchine, vado a fare un controllo
+// sullo stato del GOM prima di gom tick
 static void tick_all(cella_meccatronica *c){
+    station_status gom_stato_prima = c->gom->stato;
    //decremento i tick di lavorazione gom
     gom_tick(c->gom, c->param); 
+    if (gom_stato_prima != COOLING && c->gom->stato == COOLING){
+        log_warning_f(c->tick_corrente, "GOM in stato di raffreddamento: pezzo %d in lavorazione", c->gom->pezzo_in_lavorazione->id_pezzo);
+    }
     //decremento i tick di lavorazione pressa
     pressa_tick(c->pressa);
     //decremento i tick di lavorazione laminazione
